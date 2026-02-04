51eaf40dd891b3bfb6e92ed6e9778c7e40fd6b67
pramod1273 <pramodsimhapc48@gmail.com>
2026-02-04 12:31:32 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test {
     static void add()
     {
         int a=344333290;
-        int b=189330;
+        int b=122222289330;
         int res=a+b;
         System.out.println("the result is " +res);
     }
